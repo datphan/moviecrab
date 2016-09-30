27d3af764e1b77e1dd55948cf2ed31f5dbe861f2
Dat Phan <datphan0110@gmail.com>
2016-09-30 16:23:42 +0700

    update str len

--- app/country/models.py
+++ app/country/models.py
@@ -5,7 +5,7 @@ from ..extensions import db
 class Country(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
-    code = db.Column(db.String(3), nullable=False, unique=True)
+    code = db.Column(db.String(4), nullable=False, unique=True)
 
     @staticmethod
     def insert_countries():
